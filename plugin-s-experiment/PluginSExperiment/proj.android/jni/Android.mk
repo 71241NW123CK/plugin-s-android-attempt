12a1797f724f1d914fc49a51b591aa14714ab986
Ryan Hiroaki Tsukamoto <ryan@scopely.com>
2015-04-09 10:30:41 -0700

    changed build config JSON, Android.mk for the application project, added call to set the Java VM in main.cpp.

--- plugin-s-experiment/PluginSExperiment/proj.android/jni/Android.mk
+++ plugin-s-experiment/PluginSExperiment/proj.android/jni/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/plugin-s/publish)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -18,16 +19,22 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
-
-
+ 
+ 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
-
+ 
 # _COCOS_LIB_ANDROID_BEGIN
 # _COCOS_LIB_ANDROID_END
-
+ 
+ 
+LOCAL_WHOLE_STATIC_LIBRARIES := PluginProtocolStatic
+ 
 include $(BUILD_SHARED_LIBRARY)
-
+ 
 $(call import-module,.)
-
+ 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
+ 
+$(call import-module,protocols/android)
+#$(call import-module,cocos)
